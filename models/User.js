cfb36a79b9131bc5aa58ec53a9d2a750eaa2b6b7
mozahidsani <mozahidwebmin11d10@gmail.com>
2023-02-10 14:31:31 +0600

    sigIn SignUp done

--- models/User.js
+++ models/User.js
@@ -1,88 +1,150 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const validator = require("validator");
+const bcrypt = require("bcryptjs");
 
-const bcrypt = require('bcryptjs');
-
-const userSchema = mongoose.Schema({
-    name: {
-        type: String,
-        required: [true, "Please provide a name"],
-        minLength: [3, "Name is too short"],
-        maxLength: [100, "Name is too long"],
-        trim: true,
-        lowercase: true
-    },
+const userSchema = mongoose.Schema(
+  {
     email: {
-        type: String,
-        validate: [validator.isEmail, "Please provide a valid email"],
-        required: [true, "Please provide a email"]
+      type: String,
+      validate: [validator.isEmail, "Provide a valid Email"],
+      trim: true,
+      unique: true,
+      required: [true, "Email address is required"],
+   
     },
     password: {
-        type: String,
-        required: [true, 'Please provide a valid password'],
-        validate: {
-            validator: (value) =>
-                validator.isStrongPassword(value, {
-                    minLength: 6,
-                    minLowercase: 3,
-                    minNumbers: 1,
-                    minUppercase: 1,
-                    minSymbol: 1,
-                }),
-            message: "Password {VALUE} is not strong enough"
-        },
+      type: String,
+      required: [true, "Password is required"],
+      validate: {
+        validator: (value) =>
+          validator.isStrongPassword(value, {
+            minLength: 6,
+            minLowercase: 3,
+            minNumbers: 1,
+            minUppercase: 1,
+            minSymbols: 1,
+          }),
+        message: "Password {VALUE} is not strong enough.",
+      },
     },
     confirmPassword: {
-        type: String,
-        required: [true, "Please confirm your password"],
-        validate: {
-            validator: function (value) {
-                return value == this.password
-            },
-            message: "Password doesn't match"
+      type: String,
+      required: [true, "Please confirm your password"],
+      validate: {
+        validator: function (value) {
+          return value === this.password;
         },
+        message: "Passwords don't match!",
+      },
     },
+
     role: {
-        type: String,
-        enum: ["candidate", "hiring Manager", "admin"],
-        default: "candidate"
+      type: String,
+      enum: ["buyer", "store-manager", "admin"],
+      default: "buyer",
+    },
+
+    firstName: {
+      type: String,
+      required: [true, "Please provide a first name"],
+      trim: true,
+      minLength: [3, "Name must be at least 3 characters."],
+      maxLength: [100, "Name is too large"],
+    },
+    lastName: {
+      type: String,
+      required: [true, "Please provide a first name"],
+      trim: true,
+      minLength: [3, "Name must be at least 3 characters."],
+      maxLength: [100, "Name is too large"],
     },
     contactNumber: {
-        type: String,
-        validate: [validator.isMobilePhone,]
+      type: String,
+      validate: [validator.isMobilePhone, "Please provide a valid contact number"],
     },
-    createdAt: {
-        type: String,
-        default: Date.now(),
-        select: 0,
+
+    shippingAddress: String,
+
+    imageURL: {
+      type: String,
+      validate: [validator.isURL, "Please provide a valid url"],
     },
-    updateAt: {
-        type: String,
-        default: Date.now(),
-        select: 0
-    }
-})
+    status: {
+      type: String,
+      default: "inactive",
+      enum: ["active", "inactive", "blocked"],
+    },
 
+    confirmationToken: String,
+    confirmationTokenExpires: Date,
 
-// password hide 
+    passwordChangedAt: Date,
+    passwordResetToken: String,
+    passwordResetExpires: Date,
+  },
+  {
+    timestamps: true,
+  }
+);
+
 userSchema.pre("save", function (next) {
-    const password = this.password;
+  if (!this.isModified("password")) {
+    //  only run if password is modified, otherwise it will change every time we save the user!
+    return next();
+  }
+  const password = this.password;
 
-    const hashedPassword = bcrypt.hashSync(password);
+  const hashedPassword = bcrypt.hashSync(password);
 
-    this.password = hashedPassword;
-    this.confirmPassword = undefined;
+  this.password = hashedPassword;
+  this.confirmPassword = undefined;
 
-    next()
-})
+  next();
+});
 
-
-userSchema.methods.comparePassword = function (userPassword, hash) {
-    const isPasswordValid = bcrypt.compareSync(userPassword, hash)
-    return isPasswordValid;
+userSchema.methods.comparePassword = function (password, hash) {
+  const isPasswordValid = bcrypt.compareSync(password, hash);
+  return isPasswordValid;
 };
+
 
 const User = mongoose.model("User", userSchema);
 
 module.exports = User;
 
+/*
+
+{
+    "email": "mezba1@test.com",
+    "password": "mezba123456#A",
+    "confirmPassword": "mezba123456#A",
+    "firstName": "Mezbaul Abedin",
+    "lastName": "Forhan",
+    "shippingAddress": "944 osthir Street",
+    "presentAddress": "944 osthir Street",
+    "permanentAddress": "944 Russell Street",
+    "imageURL": "https://i.ibb.co/WnFSs9Y/unnamed.webp"
+}
+
+
+//for manager
+/*
+"name":"Manager",
+"email":"managerctg@test.com",
+"password":"mezba123456##",
+"confirmPassword":"mezba123456##",
+"firtsName":"Manager of",
+"lastName":"CTG",
+"contactNumber":"11111111111",
+"shippingAddress:"944 osthir Street",
+"division":"chattogram",
+"imageURL":"https://i.ibb.co/WnFSs9Y/unnamed.webp",
+"status":"active",
+"emergencyContactNumber":"01712345678",
+"presentAddress":"944 osthir Street",
+"permanentAddress":"944 Russell Street",
+"nationalIdImageURL":"https://i.ibb.co/WnFSs9Y/unnamed.webp",
+
+
+
+*/
